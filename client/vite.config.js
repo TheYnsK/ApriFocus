a44beb1947adebbc5d62e708c2c4b58834cc47d3
TheYnsK <theynsk.2244@gmail.com>
2026-01-11 23:41:43 +0300

    ApriFocus v1.0

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,12 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-  server: {
-    port: 5101,
-  },
-  build: {
-    outDir: 'dist', 
-    emptyOutDir: true,
-  }
-})
+  build: { outDir: "dist", emptyOutDir: true }
+});
